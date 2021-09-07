a2c3bea4a54b34b3590ec13d157e47afd538512a
mike.knebel <mike.knebel@xitaso.com>
2021-09-07 11:02:51 +0200

    Using test pond

--- src/actyx/pond-provider.service.ts
+++ src/actyx/pond-provider.service.ts
@@ -1,4 +1,5 @@
 import appManifest from "./app-manifest.json";
 import {RxPond} from "@actyx-contrib/rx-pond";
+import {Pond} from "@actyx/pond";
 
-export const pondPromise: Promise<RxPond> = RxPond.default(appManifest);
+export const pondPromise: Promise<RxPond> = Promise.resolve(RxPond.from(Pond.test()));
